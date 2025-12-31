6a6b5f9a6e7f78b454b8c133dc447c3b4e514b7b
gauravmishra00 <gauravonbeat@gmail.com>
2025-12-31 15:01:39 +0530

    css updated

--- src/components/weather.js
+++ src/components/weather.js
@@ -5,16 +5,6 @@ async function weather(lat,long){
    const data = await res.json()
    return  data.hourly.temperature_2m[0]
    
-//  fetch(url)
-//  .then((res)=>res.json())
-//  .then((res)=>{
-//     console.log(res.hourly.temperature_2m[0]);
-//     return res.hourly.temperature_2m[0]
-    
-//  })
-//  .catch((error)=>{
-//     console.log(error);   
-//  })
 }
 
 export default weather;
